b395f9b8ecde0d1506444aac0a9b1dea600ef454
sam <samliao1@auforce.com>
2024-04-26 17:14:54 +0800

    ✨ feat: 新增讀取 MSSQL 狀態功能

--- main.go
+++ main.go
@@ -9,13 +9,47 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"database/sql"
+	"fmt"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func main() {
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		"daydb-svc.h1-db-dev", "mobile_api", "a:oY%~^E+VU0", 1433, "HKNetGame_HJ")
+
+	db, err := sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT name, state_desc FROM sys.databases WHERE name = 'HKNetGame_HJ';")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name, state_desc string // 這裡的變數類型和數量應該與你的資料表欄位匹配
+		err := rows.Scan(&name, &state_desc)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("name: %s, state_desc: %s\n", name, state_desc)
+	}
+
 	router := gin.Default()
+
 	router.GET("/", func(c *gin.Context) {
-		time.Sleep(5 * time.Second)
 		c.String(http.StatusOK, "Welcome Gin Server")
+	})
+
+	router.POST("/api/Member/login", func(c *gin.Context) {
+		c.String(http.StatusOK, "login")
 	})
 
 	srv := &http.Server{
@@ -24,13 +58,13 @@ func main() {
 	}
 
 	go func() {
-		// 服务连接
+		// 服務連線
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
-	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	// 等待中斷信號以優雅地關閉服務器（設置 5 秒的超時時間）
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
